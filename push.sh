4d3e8f50bd2f721d5daebe26912f29a01c140f44
Duncan Paterson <d.paterson@me.com>
2020-07-11 17:10:23 +0200

    data update

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BRANCH=$(git symbolic-ref --short HEAD)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
@@ -8,14 +8,15 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b $BRANCH
+  git checkout -b ${BRANCH}
   git add ./*/result/ *.*
   git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  rev=$(git rev-parse --short HEAD)
   git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin $BRANCH
+  git push --quiet --set-upstream origin ${BRANCH}
 }
 
 setup_git
--- xml/test.xq
+++ xml/test.xq
@@ -1,7 +1,7 @@
 xquery version "3.1";
 
 <result> {
-for $n in 1 to 3
+for $n in 1 to 4
 return
   <element>{$n}</element>
 }
