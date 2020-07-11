3b30ff308659c59ab7f31b799dd64fc68b4775c9
Duncan Paterson <d.paterson@me.com>
2020-07-11 18:24:51 +0200

    where to place token?

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# https://www.vinaygopinath.me/blog/tech/commit-to-master-branch-on-github-using-travis-ci/
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 setup_git() {
@@ -7,17 +7,18 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  git checkout -b ${BRANCH}
+commit_files() {
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git checkout shell-push
   git add ./*/result/ *.*
   git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin HEAD:${BRANCH}
+  git remote add new-origin https://duncdrum:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+  git push new-origin shell-push --quiet
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
